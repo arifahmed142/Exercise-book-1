c54a53c50daf96c656dd57debc9a49ae166598a6
Md. Arif Ahmed <arifahmedarif142@gmail.com>
2025-05-11 23:14:22 +0600

    2-13.c

--- 2-13.c
+++ 2-13.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter Values in Order (x1, x2, y1, y2):\n");
     scanf("%d%d%d%d", &x1,&x2,&y1,&y2);
 
-    float D = sqrt ( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) );
+    float D = sqrt ( ((x2-x1)*(x2-x1))) + sqrt ( ((y2-y1)*(y2-y1)) );
 
     printf("Distance Between Two Point: %.2f", D);
 
