99e7276c1b16a578191bc6b35f0233beb282c291
Md. Arif Ahmed <arifahmedarif142@gmail.com>
2025-05-11 23:16:49 +0600

    2-13.c

--- 2-13.c
+++ 2-13.c
@@ -5,7 +5,7 @@ int main()
 {
     int x1, x2, y1, y2;
     printf("Enter Values in Order (x1, x2, y1, y2):\n");
-    scanf("%d%d%d%d", &x1,&x2,&y1,&y2);
+    scanf("%d%d%d%d", &x1,&y1,&x2,&y2);
 
     float D = sqrt ( ((x2-x1)*(x2-x1))) + sqrt ( ((y2-y1)*(y2-y1)) );
 
